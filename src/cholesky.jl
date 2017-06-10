8701f115f911583524e6fd9455bbb7bb29d28b7f
benjamin shall <benji.l.shall@gmail.com>
2017-06-11 01:48:29 +0200

    Add cholfact method

--- src/cholesky.jl
+++ src/cholesky.jl
@@ -1,41 +1,38 @@
 # Generic Cholesky decomposition for fixed-size matrices, mostly unrolled
 @inline function Base.chol(A::StaticMatrix)
     ishermitian(A) || Base.LinAlg.non_hermitian_error("chol")
-    _chol(Size(A), A, UpperTriangular)
+    C, info = _chol(Size(A), A, UpperTriangular)
+    Base.LinAlg.@assertposdef C info
 end
 
 @inline function Base.chol(A::Base.LinAlg.RealHermSymComplexHerm{<:Real,<:StaticMatrix})
-    _chol(Size(A), A.uplo == 'U' ? A.data : ctranspose(A.data), UpperTriangular)
+    C, info = _chol(Size(A), A.uplo == 'U' ? A.data : ctranspose(A.data), UpperTriangular)
+    Base.LinAlg.@assertposdef C info
 end
 
-@inline function Base.cholfact(A::StaticMatrix, uplo::Symbol, ::Type{Val{false}})
-    ishermitian(A) || Base.LinAlg.non_hermitian_error("cholfact")
-    Base.cholfact(Hermitian(A, uplo), Val{false})
-end
-
-@inline function Base.cholfact(A::StaticMatrix, uplo::Symbol = :U)
-    ishermitian(A) || Base.LinAlg.non_hermitian_error("cholfact")
-    Base.cholfact(Hermitian(A, uplo))
-end
-
-@inline function Base.cholfact(A::Base.LinAlg.RealHermSymComplexHerm{<:Real,<:StaticMatrix}, ::Type{Val{false}})
+@inline function Base.cholfact(A::Base.LinAlg.RealHermSymComplexHerm{<:Real,<:StaticMatrix}, ::Type{Val{false}}=Val{false})
     if A.uplo == 'U'
-        CU = _chol(Size(A), A.data, UpperTriangular)
+        CU, info = _chol(Size(A), A.data, UpperTriangular)
+        # TODO include info in v0.7
         Base.LinAlg.Cholesky(CU.data, 'U')
     else
-        CL = _chol(Size(A), A.data, LowerTriangular)
+        CL, info = _chol(Size(A), A.data, LowerTriangular)
+        # TODO include info in v0.7
         Base.LinAlg.Cholesky(CL.data, 'L')
     end
 end
 
-# @inline function Base.cholfact(A::Base.LinAlg.RealHermSymComplexHerm{<:Real,<:StaticMatrix}) = cholfact(A, Val{false})
+@inline function Base.cholfact(A::StaticMatrix, ::Type{Val{false}}=Val{false})
+    ishermitian(A) || Base.LinAlg.non_hermitian_error("cholfact")
+    Base.cholfact(Hermitian(A), Val{false})
+end
 
 @generated function _chol(::Size{s}, A::StaticMatrix{<:Any,<:Any,T}, ::Type{UpperTriangular}) where {s, T}
     if s[1] != s[2]
         error("matrix must be square")
     end
     n = s[1]
-    TX = promote_type(typeof(sqrt(one(T))), Float32)
+    TX = promote_type(typeof(chol(one(T), UpperTriangular)), Float32)
 
     X = [Symbol("X_$(i)_$(j)") for i = 1:n, j = 1:n]
     init = [:($(X[i,j]) = A[$i,$j]) for i = 1:n, j = 1:n]
@@ -46,8 +43,7 @@ end
         for i = 1:k-1
             ex = :($ex - $(X[i,k])'*$(X[i,k]))
         end
-        push!(code.args, :($(X[k,k]) = $ex))
-        push!(code.args, quote $(X[k,k]), info = _chol($(X[k,k]), UpperTriangular) end)
+        push!(code.args, quote $(X[k,k]), info = _chol($ex, UpperTriangular) end)
         push!(code.args, :(info == 0 || return UpperTriangular(similar_type(A, $TX)(tuple($(X...)))), info))
         if k < n
             push!(code.args, :(XkkInv = inv($(X[k,k])')))
@@ -69,38 +65,43 @@ end
     end
 end
 
-@generated function _chol(::Size{s}, a::StaticMatrix{<:Any, <:Any, T}, ::Type{LowerTriangular}) where {s, T}
+@generated function _chol(::Size{s}, A::StaticMatrix{<:Any, <:Any, T}, ::Type{LowerTriangular}) where {s, T}
     if s[1] != s[2]
         error("matrix must be square")
     end
 
     n = s[1]
-    Tx = promote_type(typeof(sqrt(one(T))), Float32)
+    TX = promote_type(typeof(chol(one(T), LowerTriangular)), Float32)
 
-    x = [i < j ? Symbol("x_0") : Symbol("x_$(i)_$(j)") for i = 1:n, j = 1:n]
+    X = [Symbol("X_$(i)_$(j)") for i = 1:n, j = 1:n]
+    init = [:($(X[i,j]) = A[$i,$j]) for i = 1:n, j = 1:n]
 
-    code = quote x_0 = $(zero(Tx)) end
+    code = quote end
     for k = 1:n
-        ex = :(a[$k, $k])
+        ex = :($(X[k,k]))
         for i = 1:k-1
-            ex = :($ex - $(x[k, i])*$(x[k, i])')
+            ex = :($ex - $(X[k,i])*$(X[k,i])')
         end
-        push!(code.args, :($(x[k, k]) = sqrt($ex)))
-        push!(code.args, :(xkkInv = inv($(x[k, k]'))))
-        for i = k+1:n
-            ex = :(a[$i, $k])
-            for j = 1:k-1
-                ex = :($ex - $(x[i, j])*$(x[k, j])'*xkkInv')
+        push!(code.args, quote $(X[k,k]), info = _chol($ex, LowerTriangular) end)
+        push!(code.args, :(info == 0 || return LowerTriangular(similar_type(A, $TX)(tuple($(X...)))), info))
+        if k < n
+            push!(code.args, :(XkkInv = inv($(X[k,k])')))
+        end
+        for j = 1:k-1
+            for i = k+1:n
+                push!(code.args, :($(X[i,k]) -= $(X[i,j])*$(X[k,j])'))
             end
-            ex = :($ex*xkkInv')
-            push!(code.args, :($(x[i, k]) = $ex*xkkInv'))
+        end
+        for i = k+1:n
+            push!(code.args, :($(X[i,k]) *= XkkInv))
         end
     end
 
     quote
         @_inline_meta
+        @inbounds $(Expr(:block, init...))
         @inbounds $code
-        @inbounds return LowerTriangular(similar_type(a, $Tx)(tuple($(x...))))
+        @inbounds return LowerTriangular(similar_type(A, $TX)(tuple($(X...)))), convert(Base.LinAlg.BlasInt, 0)
     end
 end
 
@@ -108,6 +109,30 @@ end
 function _chol(x::Number, uplo)
     rx = real(x)
     rxr = sqrt(abs(rx))
-    rval =  convert(promote_type(typeof(x), typeof(rxr)), rxr)
+    rval = convert(promote_type(typeof(x), typeof(rxr)), rxr)
     rx == abs(x) ? (rval, convert(Base.LinAlg.BlasInt, 0)) : (rval, convert(Base.LinAlg.BlasInt, 1))
+end
+
+chol(x::Number, uplo) = ((C, info) = _chol(x, uplo); Base.LinAlg.@assertposdef C info)
+
+function Base.:\(C::Base.LinAlg.Cholesky{<:Any,<:StaticMatrix}, B::StaticVecOrMat)
+    if C.uplo == 'L'
+        return LowerTriangular(C.factors)' \ (LowerTriangular(C.factors) \ B)
+    else
+        return UpperTriangular(C.factors) \ (UpperTriangular(C.factors)' \ B)
+    end
+end
+
+@inline Base.det(C::Base.LinAlg.Cholesky{<:Any,<:StaticMatrix}) = _det(Size(C.factors), C)
+
+@generated function _det(::Size{s}, C::Base.LinAlg.Cholesky{T,<:StaticMatrix}) where {s,T}
+    # TODO add following line for v0.7
+    # C.info == 0 || throw(PosDefException(C.info))
+
+    ex = :(one(real($T)))
+    for i = 1:s[1]
+        ex = :($ex*real(C.factors[$i,$i])^2)
+    end
+
+    @inbounds return ex
 end
