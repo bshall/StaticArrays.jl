7de9666ffe5f2a96be9be87f1f020a1434d22869
benjamin shall <benji.l.shall@gmail.com>
2017-07-12 17:55:03 +0200

    Start work on lufact

--- src/lu.jl
+++ src/lu.jl
@@ -1,3 +1,73 @@
+using Base.LinAlg: BlasInt
+
+struct LU{T,S<:StaticMatrix} <: Factorization{T}
+    factors::S
+    ipiv::SVector{BlasInt}
+    info::BlasInt
+    LU{T,S}(factors::StaticMatrix{T}, ipiv::SVector{BlasInt}, info::BlasInt) where {T,S} = new(factors, ipiv, info)
+end
+LU(factors::StaticMatrix{T}, ipiv::SVector{BlasInt}, info::BlasInt) where {T} = LU{T,typeof(factors)}(factors, ipiv, info)
+
+@inline lufact(A::StaticMatrix, pivot::Union{Val{false}, Val{true}} = Val(true)) = _lufact(Size(A), A, pivot)
+
+@generated function _lufact(::Size{s}, a::StaticMatrix{<:Any,<:Any,T}, ::Val{Pivot}) where {s,T,Pivot}
+    m, n = s
+    minmn = min(m,n)
+    X = [Symbol("X_$(i)_$(j)") for i = 1:m, j = 1:n]
+    ipiv = [Symbol("p_$i") for i = 1:minmn]
+
+    TX = typeof(zero(T)/one(T))
+
+    init = [:($(X[i,j]) = A[$(sub2ind(s,i,j))]) for i = 1:m, j = 1:n]
+    code = quote info = 0 end
+    for k = 1:minmn
+        if Pivot
+            ex = :(indmax(tuple($([:(abs($(X[i,k]))) for i = k:m]...))))
+            push!(code.args, :($(ipiv[k]) = $k - 1 + $ex))
+        else
+            push!(code.args, :($(ipiv[k]) = $k))
+        end
+        for kp = k:m
+            swap = quote end
+            if kp > k
+                for i = 1:n
+                    push!(swap.args, :(tmp = $(X[k,i])))
+                    push!(swap.args, :($(X[k,i]) = $(X[kp,i])))
+                    push!(swap.args, :($(X[kp,i]) = tmp))
+                end
+            end
+            push!(swap.args, :(xkkInv = inv($(X[k,k]))))
+            for i = k+1:m
+                push!(swap.args, :($(X[i,k]) *= xkkInv))
+            end
+            ex = quote
+                if $(ipiv[k]) == $kp
+                    if !iszero($(X[kp,k]))
+                        $swap
+                    elseif info == 0
+                        info = $k
+                    end
+                end
+            end
+            push!(code.args, ex)
+        end
+        update = quote end
+        for j = k+1:n
+            for i = k+1:m
+                push!(update.args, :($(X[i,j]) -= $(X[i,k])*$(X[k,j])))
+            end
+        end
+        push!(code.args, update)
+    end
+
+    quote
+        $(Expr(:meta, :inline))
+        @inbounds $(Expr(:block, init...))
+        @inbounds $code
+        @inbounds return LU(similar_type(a, $TX)(tuple($(X...))), SVector{$minmn,BlasInt}(tuple($(ipiv...))), convert(BlasInt, info))
+    end
+end
+
 # LU decomposition
 function lu(A::StaticMatrix, pivot::Union{Type{Val{false}},Type{Val{true}}}=Val{true})
     L,U,p = _lu(Size(A), A, pivot)
